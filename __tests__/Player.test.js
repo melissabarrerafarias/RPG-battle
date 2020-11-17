e21d50ee3aa92f4bbf2312de932c7780a98c1c4b
melissikiss <melissikiss@gmail.com>
2020-11-16 18:03:57 -0800

    added inventory to player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,11 @@
 const Player = require('../lib/Player');
 
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
+
 test('creates a player object', () => {
     const player = new Player('Dave');
 
@@ -7,5 +13,16 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
 
+
+// A good test runs in isolation. Mocks allow us to fake assumed data, which allows the test at hand to focus only on the logic it cares about. 
+
+// const Potion = require('../lib/Potion');
+// jest.mock('../lib/Potion');
+// the require() line imports the Potion() constructor into the test, establishing Potion as a usable variable (otherwise new Potion() would throw and error). Then jest.mock() mocks/replaces
+// the constructor's implementation with our faked data. 
